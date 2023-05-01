49fa8c60ddeff288696d4d0d6de498780b4567ca
krzylesniewski <krzysztof.lesniewski.wroc@gmail.com>
2023-05-01 02:38:17 +0200

    Menu pause bug fix

--- Assets/Script/PauseMenu.cs
+++ Assets/Script/PauseMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -9,9 +10,10 @@ public class PauseMenu : MonoBehaviour
     [SerializeField] public GameObject _pauseMenuUI;
 
     // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
         if (Input.GetKeyDown(KeyCode.Space)) {
+
             Debug.Log("Spacja");
         }
 
@@ -36,11 +38,13 @@ public class PauseMenu : MonoBehaviour
         _pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         _isGamePaused = false;
+        InputSystem.EnableDevice(Mouse.current);
     }
     void Pause()
     {
         _pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         _isGamePaused = true;
+        InputSystem.DisableDevice(Mouse.current);
     }
 }
